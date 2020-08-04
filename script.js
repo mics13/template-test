3a0a7ad2c82860e4ff2aacf88a8d1c1b75507f56
mics13 <micsiu13@gmail.com>
2020-08-04 19:10:29 +0800

    QG

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-console.log('testing');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+// Get Quote from API
+async function getQuote() {
+  showLoadingSpinner();
+  const proxyUrl = 'https://shielded-journey-30799.herokuapp.com/';
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    console.log(data);
+    // if quoteAuthor is blank, add 'Unknown'
+    if (data.quoteAuthor === ''){
+      authorText.innerText = 'Unknow';
+    } else{
+      authorText.innerText = data.quoteAuthor;
+    }
+    // reduce font size for long quote
+    if (data.quoteText.length > 120){
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
+    removeLoadingSpinner();
+
+  } catch (error) {  
+  getQuote();
+  } 
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=\"${quote}\" - ${author}`;
+  window.open(twitterUrl, '_blank_');
+}
+
+// Event Listener
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuote();
